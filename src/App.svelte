ccf01e5d74732a78acb83dc0eca1db788aa09db9
Kevin Yu <kevinyu567@gmail.com>
2021-10-11 21:26:02 -0700

    Add Heading, favicon

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,19 @@
 <script lang="ts">
-    import {fade, draw, fly} from 'svelte/transition';
-
     import About from './About.svelte';
     import FAQ from './FAQ.svelte';
-
-    let visible = false;
-
-    setTimeout(() => visible = true, 100)
+    import Heading from "./Heading.svelte";
 </script>
 
-<section id="about">
-    {#if visible}
-        <div class="title-wrapper">
-            {#each 'GunnHacks 8.0' as char, i}
-                <span class="title" in:fly="{{y: -20, delay: 1000 + i * 100, duration: 800}}">{char}</span>
-            {/each}
-        </div>
-    {/if}
-    <p>The eight iteration of GunnHacks, Gunn High School's annual hackathon organized by United Computations.</p>
-</section>
-
+<Heading />
 <About />
 <FAQ />
 
 <link href="https://fonts.googleapis.com/css?family=Overpass:100,400" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
 
-<style lang="scss">
-  :global(body) {
+<style global lang="scss">
+  body {
     background-color: rgb(17, 24, 39);
     color: white;
-  }
-
-  #about {
-    text-align: center;
-
-    .title-wrapper {
-      .title {
-        font-size: 10rem;
-        transform: translate(-50%, -50%);
-        font-family: 'Overpass';
-        letter-spacing: 0.12em;
-        color: white;
-        font-weight: 400;
-        will-change: filter;
-      }
-    }
   }
 </style>
---
+++ src/Heading.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import {fade, draw, fly} from 'svelte/transition';
+
+    let visible = false;
+    setTimeout(() => visible = true, 100)
+</script>
+
+<div id="header-image"></div>
+<section id="heading">
+    {#if visible}
+        <div class="title-wrapper">
+            {#each 'GunnHacks 8.0' as char, i}
+                <span class="title" in:fly="{{y: -20, delay: 1000 + i * 100, duration: 800}}">{char}</span>
+            {/each}
+        </div>
+    {/if}
+    <p>The eight iteration of GunnHacks, Gunn High School's annual hackathon organized by United Computations.</p>
+</section>
+
+<style lang="scss">
+  #header-image {
+    position: fixed;
+    background-image: url('../bg.svg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+  }
+
+  #heading {
+    text-align: center;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .title-wrapper {
+      .title {
+        font-size: 10rem;
+        transform: translate(-50%, -50%);
+        font-family: 'Overpass';
+        letter-spacing: 0.12em;
+        color: white;
+        font-weight: 400;
+        will-change: filter;
+      }
+    }
+  }
+</style>
