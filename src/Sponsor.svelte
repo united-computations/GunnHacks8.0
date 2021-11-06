30480603a5879002c2381aa3f0988c04e91150ba
Kevin Yu <kevinyu567@gmail.com>
2021-11-05 22:13:31 -0700

    Add our first sponsor, wolfram!

---
+++ src/Sponsor.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let href: string;
+    export let src: string;
+    export let alt: string;
+</script>
+
+<div>
+    <a href={href}>
+        <img src={src} alt={alt}>
+    </a>
+</div>
+
+<style lang="scss">
+  $max-height: 5.5rem;
+  $max-width: 18rem;
+
+  div {
+    flex: 1 1 $max-width;
+
+    img {
+      max-height: $max-height;
+      max-width: $max-width;
+      padding: 0.5rem 1rem;
+    }
+  }
+</style>
--- src/Sponsors.svelte
+++ src/Sponsors.svelte
@@ -1,11 +1,26 @@
 <script>
     import Section from './Section.svelte';
+    import Sponsor from './Sponsor.svelte';
 </script>
 
 <Section title="Sponsors">
     <p>
-        To sponsor our event, email us at <a href="mailto:ghshacks@gmail.com">ghshacks@gmail.com.</a>
-        Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="docs/sponsorship_prospectus.pdf">here.</a>
+        To sponsor our event, email us at <a href="mailto:ghshacks@gmail.com">ghshacks@gmail.com</a>.
+        Find our sponsorship prospectus <a target="_blank" rel="noopener noreferrer" href="docs/sponsorship_prospectus.pdf">here</a>.
     </p>
-    <!-- Sponsors go here when we get some! -->
+
+    <div class="sponsors-content">
+        <Sponsor href="https://www.wolfram.com" src="sponsors/wolfram.svg" alt="wolfram" />
+    </div>
 </Section>
+
+<style lang="scss">
+  .sponsors-content {
+    margin: 0 auto;
+    padding-top: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    text-align: center;
+  }
+</style>
