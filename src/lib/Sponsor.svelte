df0338ddf2dd5a5148eb39e82c607e83e53e1c3c
Kevin Yu <kevinyu567@gmail.com>
2021-11-23 14:49:53 -0800

    Fix sponsor images on phones

--- src/lib/Sponsor.svelte
+++ src/lib/Sponsor.svelte
@@ -13,14 +13,23 @@
 <style lang="scss">
   $max-height: 5.5rem;
   $max-width: 18rem;
+  $max-width-small: 16rem;
 
   div {
     flex: 1 1 $max-width;
+
+    @media (max-width: 640px) {
+      flex-basis: $max-width-small;
+    }
 
     img {
       max-height: $max-height;
       max-width: $max-width;
       padding: 0.5rem 1rem;
+
+      @media (max-width: 640px) {
+        max-width: $max-width-small;
+      }
     }
   }
 </style>
