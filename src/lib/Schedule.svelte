17f4e937481e872b441636c91014ad8311dbd1ee
Kevin Yu <kevinyu567@gmail.com>
2022-01-17 12:23:08 -0800

    Add schedule

--- src/lib/Schedule.svelte
+++ src/lib/Schedule.svelte
@@ -1,7 +1,41 @@
 <script>
     import Section from './Section.svelte';
+    import ScheduleColumn from './ScheduleColumn.svelte';
+
+    const feb5 = [
+        ['12:30 PM', 'Opening Ceremony'],
+        ['1:30 PM', 'Team formation activity begins'],
+        ['2:00 PM', 'Hacking begins'],
+        ['2:00 PM', 'Intro to Python Workshop'],
+        ['4:00 PM', 'React Workshop'],
+    ];
+    const feb6 = [
+        ['9:00 AM', 'Networking Workshop'],
+        ['2:00 PM', 'Hacking ends'],
+        ['2:00 PM', 'Judging starts'],
+        ['4:30 PM', 'Judging ends'],
+        ['5:00 PM', 'Closing ceremony'],
+    ];
 </script>
 
 <Section red title="Schedule">
-    <p>To be announced soon.</p>
+    <div class="schedule-blocks">
+        <ScheduleColumn date="February 5th" schedule={feb5} />
+        <ScheduleColumn date="February 6th" schedule={feb6} />
+    </div>
+    <p>All times are in PST.</p>
 </Section>
+
+<style lang="scss">
+  .schedule-blocks {
+    display: flex;
+    gap: 1rem;
+  }
+
+  /*
+  p {
+    font-style: italic;
+    color: rgba(255, 255, 255, 0.7);
+  }
+  */
+</style>
---
+++ src/lib/ScheduleColumn.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    export let date: string;
+    export let schedule: [string, string][];
+</script>
+
+<div class="schedule-column">
+    <h3>{date}</h3>
+    <div class="schedule-grid">
+        <strong>Time</strong>
+        <strong>Event</strong>
+
+        {#each schedule as [time, name]}
+            <span>{time}</span>
+            <span>{name}</span>
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+  .schedule-column {
+    flex-basis: 50%;
+
+    h3 {
+      text-align: center;
+      margin: 1rem 0;
+      font-size: 1.33rem;
+      letter-spacing: 0.025em;
+    }
+
+    .schedule-grid {
+      display: grid;
+      grid-template-columns: auto auto;
+      text-align: left;
+
+      > * {
+        padding: 0.5rem;
+
+        &:nth-last-child(n+3) {
+          border-bottom: 1px solid rgba(255, 255, 255, 0.7);
+        }
+      }
+    }
+  }
+</style>
